5b302cbeea33a358f28b73042e32a3c6d5e37f54
unknown <ajaypalsingh82775@gmail.com>
2026-03-20 19:25:02 +0530

    Final fix for Fiverr circle icon and larger Github icon scale

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -34,19 +34,18 @@ const LinkedinColorIcon = ({ size = 32 }) => (
 );
 
 const GithubColorIcon = ({ size = 32 }) => (
-  <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="24" height="24" rx="12" fill="#181717"/>
-    <path fillRule="evenodd" clipRule="evenodd" d="M12 4C7.58 4 4 7.58 4 12c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38.0-.19-.01-.82-.01-1.49-2.22.48-2.69-.94-2.69-.94-.36-.92-.89-1.16-.89-1.16-.73-.5.05-.49.05-.49.8.06 1.22.82 1.22.82.71 1.22 1.86.87 2.32.66.07-.51.28-.5 1.07-1.78.2-3.64.89-3.64 3.95 0 .87.31 1.59.82 2.15-.08.2-.36 1.02.08 2.12 0 0 .67-.21 2.2-.82a7.42 7.42 0 0 1 4 0c1.53 1.04 2.2.82 2.2.82.44-1.1.16-1.92.08-2.12.51-.56.82-1.28.82-2.15 0-3.07-1.87-3.75-3.65-3.95.29-.25.54-.73.54-1.48 0-1.07-.01-1.93-.01-2.2 0-.21.15-.46.55-.38A8.01 8.01 0 0 0 20 12c0-4.42-3.58-8-8-8z" fill="white"/>
+  <svg width={size} height={size} viewBox="0 0 24 24" fill="#181717" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.477 2 2 6.48 2 12c0 4.42 2.865 8.166 6.839 9.49.5.092.682-.217.682-.482 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.455-1.152-1.11-1.46-1.11-1.46-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.529 2.341 1.087 2.91.831.092-.646.35-1.087.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.56 9.56 0 0112 6.844c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.025 2.747-1.025.546 1.377.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.163 22 16.418 22 12c0-5.52-4.477-10-10-10z"/>
   </svg>
 );
 
 const FiverrColorIcon = ({ size = 32 }) => (
   <svg width={size} height={size} viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="50" cy="50" r="50" fill="#1dbf73"/>
-    <g transform="translate(24, 28) scale(2.5)">
-      <path d="M18.6 3.9h-2.6c-1 0-1.4.5-1.4 1.3v2H17v3h-2.4v6.8H11v-6.8H8.8v-3H11V4.8c0-2.3 2-3.8 4.6-3.8h3v2.9z" fill="white" />
-      <circle cx="4.8" cy="4.8" r="1.8" fill="white" />
-      <path d="M3.3 7.2h3v10.6h-3z" fill="white" />
+    <g transform="translate(10, -2)">
+      <path d="M34 25 A12 12 0 0 1 46 37 V45 H54 V55 H46 V82 H32 V55 H24 V45 H32 V37 A2 2 0 0 0 30 35 H24 V23 H34 Z" fill="white" />
+      <circle cx="68" cy="29" r="7" fill="white" />
+      <rect x="61" y="45" width="14" height="37" fill="white" />
     </g>
   </svg>
 );
@@ -76,8 +75,8 @@ const Footer: React.FC<FooterProps> = ({ onPrivacyClick, onTermsClick, onRefundC
               {[
                 { icon: <InstagramColorIcon size={32} />, href: "https://www.instagram.com/vertexadigital.dev/" },
                 { icon: <LinkedinColorIcon size={32} />, href: "https://www.linkedin.com/company/vertexa-digital-agency" },
-                { icon: <GithubColorIcon size={32} />, href: "https://github.com/Ajaypal110" },
-                { icon: <FiverrColorIcon size={32} />, href: "https://www.fiverr.com/vertexa_digital" },
+                { icon: <GithubColorIcon size={36} />, href: "https://github.com/Ajaypal110" },
+                { icon: <FiverrColorIcon size={36} />, href: "https://www.fiverr.com/vertexa_digital" },
               ].map((social) => (
                 <a 
                   key={social.href}
