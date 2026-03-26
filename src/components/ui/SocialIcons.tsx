a12b685f291bf1cc95ad878430d41ae91558af38
unknown <ajaypalsingh82775@gmail.com>
2026-03-26 18:25:51 +0530

    refactor: move favicons to root for better compatibility

--- src/components/ui/SocialIcons.tsx
+++ src/components/ui/SocialIcons.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 export const InstagramColorIcon = ({ size = 32 }) => (
   <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
